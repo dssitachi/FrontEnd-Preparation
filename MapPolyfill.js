3df56cb64bd9ab0999516e03411d9ac330da70f4
Dipesh Ss <dipeshshivrame@gmail.com>
2022-06-12 12:26:29 +0530

    Map polyfill

---
+++ MapPolyfill.js
@@ -0,0 +1,16 @@
+const c = [1, 2, 3, 5]
+
+const d = c.map(x => x * 2)
+
+const f = c.myMap(function(x) {
+    return x + 2;
+})
+
+console.log(f)
+
+Array.prototype.myMap = function(callbackfn) {
+    let obj = this
+    let res = []
+    obj.forEach(x => res.push(callbackfn(x)))
+    return res;
+}
