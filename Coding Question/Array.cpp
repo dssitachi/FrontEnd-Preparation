e58ccd6fab97866e06776734f8f79d9ae38fccef
Dipesh Ss <dipeshshivrame@gmail.com>
2022-07-20 00:46:34 +0530

    Longest consecutive subsequence

--- Coding Question/Array.cpp
+++ Coding Question/Array.cpp
@@ -17,3 +17,27 @@ vector<int> twoSum(vector<int>& nums, int target) {
     }
     return res;
 }
+
+/* Longest Consecutive Sequence
+    TC : O(n)
+  
+    Input: nums = [100,4,200,1,3,2]
+    Output: 4
+    Explanation: The longest consecutive elements sequence is [1, 2, 3, 4]. Therefore its length is 4.
+*/
+
+unordered_set<int> s;
+
+for(auto x: nums) s.insert(x);
+int res = 0;
+for(int i = 0; i < nums.size(); i++) {
+    if(s.find(nums[i] - 1) != s.end()) continue;
+    int cnt = 1;
+    int c = nums[i] + 1;
+    while(s.find(c) != s.end()) {
+        cnt++;
+        c++;
+    }
+    res = max(res, cnt);
+}
+return res;
